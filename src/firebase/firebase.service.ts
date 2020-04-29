5ffca85702e16fe5c14d4a4d6b3067be295fe825
Mimi Scott <mimiscott94@gmail.com>
2020-04-29 19:37:04 -0400

    Created generic FirebaseService - also a user service that extends it

---
+++ src/firebase/firebase.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({ providedIn: "root" })
+export class FirebaseService {
+    constructor() { }
+
+    protected get() {}
+
+    protected put() {}
+
+    protected edit() {}
+
+    protected delete() {}
+}
---
+++ src/firebase/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { FirebaseService } from './firebase.service';
+
+@Injectable({ providedIn: "root"})
+export class UserService extends FirebaseService {
+
+    constructor() {
+        super();
+    }
+}
