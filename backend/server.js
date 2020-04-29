ef648832e33d0eb29c617179fc26e07de66d235f
Mimi Scott <mimiscott94@gmail.com>
2020-04-28 22:11:23 -0400

    changed name of frontend folder to client

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to Second Helpings.' });
+  res.json({ message: 'Welcome to Second Helpings Backend.' });
 });
 
 const db = require('./app/models');
@@ -26,6 +26,7 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log('Dropped and re-sync db.');
 });
 
+require('./app/routes/user.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
